1a33f488e00a4706f71354df7854ae9c3f2a1910
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-05-01 20:10:13 +0600

    fix type

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ sudo pacman -Syy && sudo pacman -Syyu
 print "Installing Yay AUR Helper"
 sudo pacman -S yay
 print "Installing required packages..."
-yay -S git neofetch neovim zsh starship lsb wget curl chromium visual-studio-code-bin seahorse
+yay -S git neofetch neovim zsh starship lsd wget curl chromium seahorse
 print "Finished installing packages!"
