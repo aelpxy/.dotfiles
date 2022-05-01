c3caf4ed1c22bd06f4c9d4cb854a2f13914a37e4
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-05-01 20:13:43 +0600

    even more

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ sudo pacman -Syy && sudo pacman -Syyu
 print "Installing Yay AUR Helper"
 sudo pacman -S yay
 print "Installing required packages..."
-yay -S git neofetch neovim zsh starship lsd wget curl chromium seahorse
+yay -S git neofetch neovim zsh starship lsd wget curl chromium seahorse noto-fonts-emoji
 print "Finished installing packages!"
