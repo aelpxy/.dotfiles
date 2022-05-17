35980eef150b896f1b6a4c99b18821f0ed3e0619
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-05-17 20:18:40 +0100

    wha

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 print "Checking for updates..."
+
 sudo pacman -Syy && sudo pacman -Syyu
+
 print "Installing Yay AUR Helper"
+
 sudo pacman -S yay
+
 print "Installing required packages..."
+
 yay -S git neofetch neovim zsh starship lsd wget curl chromium seahorse noto-fonts-emoji
 print "Finished installing packages!"
+
 cd ~/ && git@github.com:Aelpxy/.dotfiles.git
