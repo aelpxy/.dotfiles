8706a2d17182985e2bf180416e6d301e62391e22
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-08-28 19:46:25 +0600

    added direct curl installation link

--- setup.sh
+++ setup.sh
@@ -8,7 +8,10 @@ sudo pacman -S yay
 
 print "Installing required packages..."
 
-yay -S git neofetch neovim zsh starship lsd wget curl chromium seahorse noto-fonts-emoji
+yay -S git neofetch neovim zsh lsd wget curl chromium seahorse noto-fonts-emoji ttf-nerd-fonts-symbols
+
+curl -sS https://starship.rs/install.sh | sh
+
 print "Finished installing packages!"
 
 cd ~/ && git@github.com:Aelpxy/.dotfiles.git
