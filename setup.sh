673dcf72eb5147aab6ab12d6728343b521480330
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2023-01-31 14:38:47 +0000

    feat: automatically fetch and put the configs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+CONFIG_DIR="/home/aelpxy/.config/"
+
 sudo pacman -Syy
 sudo pacman -Syyu
 sudo pacman -S yay
@@ -5,7 +7,14 @@ sudo pacman -S git neofetch neovim fish lsd wget curl chromium seahorse noto-fon
 
 curl -sS https://starship.rs/install.sh | sh
 
+# Instant setup
 mkdir ~/Workspace
-mkdir ~/.ssh
+mkdir ~/.ssh && chmod 700 ~/.ssh
 rm -rf ~/.bash_*
-cd ~/ && git@github.com:Aelpxy/.dotfiles.git
+cd $CONFIG_DIR && mkdir alacritty neofetch btop fish
+
+# I don't know if there's a better way than this :/
+curl -L https://raw.githubusercontent.com/Aelpxy/.dotfiles/main/.config/starship.toml >> $CONFIG_DIR/starship.toml
+curl -L https://raw.githubusercontent.com/Aelpxy/.dotfiles/main/.config/neofetch/config.conf >> $CONFIG_DIR/neofetch/config.conf
+curl -L https://raw.githubusercontent.com/Aelpxy/.dotfiles/main/.config/fish/config.fish >> $CONFIG_DIR/fish/config.fish
+curl -L https://raw.githubusercontent.com/Aelpxy/.dotfiles/main/.config/btop/btop.conf $CONFIG_DIR/btop/btop.conf
