b96c462ff7146346fc967267d9024487bf4769b0
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2023-02-21 21:17:49 +0600

    feat: better clean_cache

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,16 +17,16 @@ alias upgrade="yay -Syy && yay -Syyu"
 alias update="yay -Syy"
 alias install="yay -S"
 alias uninstall="yay -Rnscd"
-alias cs="yay -Sc"
 alias search="yay -Ss"
 alias localsearch="yay -Qs"
+alias f="find . -name"
 
 alias ls='lsd -al --color=always'
 alias la='lsd -a --color=always'
 alias ll='lsd -l --color=always'
 
 alias cls="clear"
-alias cc="rm -rf ~/.xsession-errors.old && rm -rf ~/.xsession-errors && rm -rf ~/.cache && rm -rf ~/.wget-hsts && sudo rm -rf ./tmp/ && rm -rf ~/.npm"
+alias cc='clean_cache'
 alias e="exit"
 alias q="exit"
 alias bashtop="btop"
@@ -38,6 +38,18 @@ alias reload="source ~/.config/fish/config.fish"
 function weather
         command curl -s https://wttr.in/$argv
 end
+
+clean_cache () {
+  rm -rf ~/.xsession-errors.old
+  rm -rf ~/.xsession-errors
+  rm -rf ~/.cache
+  rm -rf ~/.wget-hsts
+  sudo rm -rf ./tmp/
+  rm -rf ~/.npm
+  sudo journalctl --rotate   # rotate the journal files
+  sudo journalctl --vacuum-time=1s   # delete all journal files older than 1 second
+  yay -Scc --noconfirm   # remove cached package files from pacman
+}
 
 set -x GPG_TTY (tty)
 set -x PATH $PATH:/usr/local/go/bin
