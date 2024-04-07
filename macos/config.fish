a7604a4a968ce98afaea6802911514eb87dfaeef
Aelpxy <aelpxy@protonmail.com>
2024-04-07 10:38:11 +0600

    feat: add macos fish config

---
+++ macos/config.fish
@@ -0,0 +1,43 @@
+if status is-interactive
+    set -g fish_greeting
+end
+
+# Aliases
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias work="cd ~/Workspace"
+alias ls='lsd -al --color=always'
+alias la='lsd -a --color=always'
+alias ll='lsd -l --color=always'
+alias cls="clear"
+alias e="exit"
+alias q="exit"
+alias bashtop="btop"
+alias htop="btop"
+alias shutdown="sudo shutdown -h now"
+alias vi="nvim"
+alias vim="vi"
+alias reload="source ~/.config/fish/config.fish"
+alias cc='clean_cache'
+alias npm="pnpm"
+alias header="curl -I"
+
+set -x GPG_TTY (tty)
+fish_add_path "/opt/homebrew/bin/"
+
+starship init fish | source
+
+function clean_cache
+    rm -rf ~/.xsession-errors.old
+    rm -rf ~/.xsession-errors
+    rm -rf ~/.wget-hsts
+    brew autoremove
+    brew cleanup --prune=all
+    history clear
+end
+
